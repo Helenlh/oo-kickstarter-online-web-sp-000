3f4752b6768c4d8349cc51d69cbcde82d310fe69
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-21 04:07:11 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,16 @@
 require 'pry'
 
 class Backer
-  attr_accessor :backed_projects, :name 
+  attr_accessor :backed_projects, :name
   
   def initialize(name)
     @backed_projects = []
     @name = name 
   end 
   
-  def back_project(name) 
-    @backed_projects << name
-    add_backer 
+  def back_project(project) 
+    @backed_projects << project
+    project.backer
   end
 end 
  
